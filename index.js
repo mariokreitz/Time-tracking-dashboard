d27f677f22a49b90e4ebf9883b19143ffd40495d
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-11 11:03:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import { fetchData } from "./lib/utils.js";
 import CardGrid from "./component/CardGrid.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  console.info("DOM loaded");
   const data = await fetchData("data.json");
-  console.log(data);
 
   let time = "daily";
   CardGrid(data, time);
