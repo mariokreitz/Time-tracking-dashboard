1b14664b9522903485029a83e398e7d0721b8b56
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-11 08:03:23 +0200

    Update Card.js

--- component/ui/Card.js
+++ component/ui/Card.js
@@ -1,14 +1,15 @@
 const Card = (card, time = "daily") => {
   return /*html*/ `
-    <div class="card">
-      <div class="card-header">
-        <h2 class="card-title">${card.title}</h2>
-      </div>
+    <article class="card">
+      <header data-category="${card.title.toLowerCase()}" class="card-header">
+        <img src="${card.image}" alt="${card.title}">
+      </header>
       <div class="card-body">
+      <h2 class="card-title">${card.title}</h2>
         <p class="card-current">${card.timeframes[time].current}hrs</p>
         <p class="card-previous">Last Day - ${card.timeframes[time].previous}hrs</p>
       </div>
-    </div>
+    </article>
   `;
 };
 
