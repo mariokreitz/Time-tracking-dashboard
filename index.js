2673fb62a2419a7b4e88916890f7f8c19ae7d539
Mario Kreitz <Mario.Kreitz@web.de>
2025-04-11 07:35:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import { fetchData } from "./lib/utils.js";
-import Card from "./template/Card.js";
+import CardGrid from "./component/CardGrid.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   console.info("DOM loaded");
   const data = await fetchData("data.json");
-  console.log(data);
 
-  data.forEach((item) => {
-    const card = Card(item);
-    document.querySelector(".cards").insertAdjacentHTML("beforeend", card);
+  let time = "daily";
+  CardGrid(data, time);
+
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      buttons.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
+      time = button.id;
+      CardGrid(data, time);
+    });
   });
 });
